791a9966e24d1040f9b8bfa6f3a4a97d40392c90
front_박찬현 <happyyquokka@cashwalk.io>
2021-09-02 11:59:02 +0900

    goolge login logic updated

--- web/google/router.js
+++ web/google/router.js
@@ -4,18 +4,13 @@ import authConfig from '../../google-auth.json';
 
 const router = Router();
 
-const getAuthenticatedClient = () => {
-  const authenticatedClient = new OAuth2Client(
-    authConfig.web.client_id,
-    authConfig.web.client_secret,
-    authConfig.web.redirect_uris[0]
-  );
+const client = new OAuth2Client(
+  authConfig.web.client_id,
+  authConfig.web.client_secret,
+  authConfig.web.redirect_uris[0]
+);
 
-  return authenticatedClient;
-};
-
-const getAuthorizeUrl = () => {
-  const client = getAuthenticatedClient();
+const getAuthorizeUrl = (client) => {
   const authorizeUrl = client.generateAuthUrl({
     access_type: 'offline',
     scope: ['email', 'profile'],
@@ -24,22 +19,84 @@ const getAuthorizeUrl = () => {
   return authorizeUrl;
 };
 
-const getTokenInfo = async (code) => {
-  const client = getAuthenticatedClient();
+const getTokenInfo = async (client, code) => {
   const authInfo = await client.getToken(code);
   return authInfo.tokens;
 }
 
-router.get('/login', (req, res, err) => {
-  const googleLoginUrl = getAuthorizeUrl();
-  res.redirect(googleLoginUrl + '/google');
+const verifyIdToken = async (client, token) => {
+  const res = await client.verifyIdToken({
+    idToken: token,
+    audience: authConfig.web.client_id,
+  });
+
+  const payload = res.getPayload();
+  console.log(payload)
+
+  return payload;
+}
+
+const getBearerToken = (auth) => {
+  if (typeof auth !== 'string') return null;
+  const splitAuth = auth.split(' ');
+  if (splitAuth[0] !== 'Bearer') return null;
+  return splitAuth[1];
+}
+
+const redirectToGoogleLogin = (client, res) => {
+  const googleLoginUrl = getAuthorizeUrl(client);
+  res.redirect(googleLoginUrl);
+}
+
+router.get('/login', async (req, res, err) => {
+  const { authorization } = req.headers;
+  const token = getBearerToken(authorization);
+
+  try {
+    if (token) {
+      const ticketPayload = await verifyIdToken(client, token);
+      // TODO: check user details with verified info in the database
+      res.send(ticketPayload);
+    } else {
+      throw 'NO TOKEN';
+    }
+  } catch (e) {
+    console.error(e);
+    redirectToGoogleLogin(client, res);
+  }
+
+  {/**
+    EASY WAY TO VERIFY ID_TOKEN
+    endpoint: https://oauth2.googleapis.com/tokeninfo?id_token=${ID_TOKEN}
+    response:
+      success: same as payload above
+      fail: {
+        "error": "invalid_token",
+        "error_description": "Invalid Value"
+      }
+
+    *** NOT RECOMMENDED FOR PRODUCTION ***
+
+    referenced: https://developers.google.com/identity/sign-in/web/backend-auth
+  */}
 });
 
 router.get('/auth/callback', async (req, res, err) => {
   const code = req.query.code;
-  const tokenInfo = await getTokenInfo(code);
-  console.log(tokenInfo);
-  res.redirect('http://localhost:5001/google');
+
+  try {
+    const tokenInfo = await getTokenInfo(client, code);
+
+    client.setCredentials(tokenInfo);
+    const { credentials } = client;
+    // TODO: save user info into database
+
+    if (!credentials) throw 'token is not valid!!'
+    res.redirect('http://localhost:5001');
+  } catch (e) {
+    console.error(e);
+    redirectToGoogleLogin(client, res);
+  }
 });
 
 router.get('/', (req, res, err) => {
